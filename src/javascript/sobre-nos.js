6f0aa8a666691bab19fdc481d9e6f72a773be945
Eduardo-Amaral <eduardo.lopes.amaral@outlook.com>
2022-08-28 20:48:49 -0300

    dark mode

---
+++ src/javascript/sobre-nos.js
@@ -0,0 +1,4 @@
+// Evento 6: Exibe um alerta com mensagem na tela
+document.getElementById("fale-conosco").addEventListener("submit", () => {
+  alert("Estaremos analisando o seu perfil, entraremos em contato em breve");
+});
